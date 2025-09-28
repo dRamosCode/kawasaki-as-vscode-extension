3fa44a87d424d2dc3d33e9d85370e8d3bf920feb
Diego Ramos <>
2025-09-28 13:15:39 +0200

    🐛 Fix GOTO jumps in compact IF instructions

--- src/programDefinition.js
+++ src/programDefinition.js
@@ -1,73 +1,87 @@
 const vscode = require("vscode");
 
-const programDefinition = function(document, position, token) {
-    // Get the word under the cursor
-    const wordRange = document.getWordRangeAtPosition(position);
-    const word = document.getText(wordRange);
-    
-    // Get the previous word
-    const previousWordLine = document.lineAt(position);
-    const previousWord = previousWordLine.text.trim().split(' ')[0];
+const programDefinition = function (document, position, token) {
+  // Get the word under the cursor
+  const wordRange = document.getWordRangeAtPosition(position);
+  const word = document.getText(wordRange);
 
-    // Define variables
-    let regex;
-    let match;
-    let location;
+  // Declare previousWord
+  let previousWord;
 
-    // GOTO definitions
-    if (previousWord=="GOTO"){
-        let programText="";
-        let whileIndex = 0;
-        let definitionFound = false;
+  // Get the current word
+  const currentWordRange = document.getWordRangeAtPosition(position);
+  if (currentWordRange) {
+    const currentWord = document.getText(currentWordRange);
 
-        // Find nearest label definition upwards
-        while (programText != word+":" && programText != ".PROGRAM") {
-            programText = document.lineAt(position.line - whileIndex).text.trim().split(' ')[0];
-            if(programText != word+":"){
-                whileIndex++;
-            }
-            else{
-                definitionFound = true;
-            }
-        }
-        if (definitionFound==false){
-            whileIndex = 0;
-            // Find nearest label definition downwards
-            while (programText != word+":" && programText != ".END") {
-                programText = document.lineAt(position.line - whileIndex).text.trim().split(' ')[0];
-                if(programText != word+":"){
-                    whileIndex--;
-                }
-                else{
-                    definitionFound = true;
-                }
-            }
-        }
-        // Reset label location if definition was not found
-        if (definitionFound == false){
-            whileIndex = 0;
-        }
-        // Return label location
-        let myRange = new vscode.Range(document.lineAt(position.line - whileIndex).range.start,document.lineAt(position.line - whileIndex).range.end);
-        location = new vscode.Location(document.uri, myRange);
-        return location;
+    // Get text from start of line to the beginning of the current word
+    const lineStart = new vscode.Position(currentWordRange.start.line, 0);
+    const textBefore = document.getText(new vscode.Range(lineStart, currentWordRange.start));
+
+    // Match the last word before the current one
+    const match = textBefore.match(/(\w+)\W*$/);
+    previousWord = match ? match[1] : undefined;
+  }
+
+  // Define variables
+  let regex;
+  let match;
+  let location;
+
+  // GOTO definitions
+  if (previousWord == "GOTO") {
+    let programText = "";
+    let whileIndex = 0;
+    let definitionFound = false;
+
+    // Find nearest label definition upwards
+    while (programText != word + ":" && programText != ".PROGRAM") {
+      programText = document
+        .lineAt(position.line - whileIndex)
+        .text.trim()
+        .split(" ")[0];
+      if (programText != word + ":") {
+        whileIndex++;
+      } else {
+        definitionFound = true;
+      }
     }
-    // Program definitions
-    else{
-        // Find program definition
-        regex = new RegExp(`PROGRAM\\s+${word}\\b`, 'g');
-        match = regex.exec(document.getText());
-        if (match) {
-            const range = new vscode.Range(
-                document.positionAt(match.index),
-                document.positionAt(match.index + match[0].length)
-            );
-            // Return definition location
-            const location = new vscode.Location(document.uri, range);
-            return location;
+    if (definitionFound == false) {
+      whileIndex = 0;
+      // Find nearest label definition downwards
+      while (programText != word + ":" && programText != ".END") {
+        programText = document
+          .lineAt(position.line - whileIndex)
+          .text.trim()
+          .split(" ")[0];
+        if (programText != word + ":") {
+          whileIndex--;
+        } else {
+          definitionFound = true;
         }
+      }
     }
-    return null;
-}
+    // Reset label location if definition was not found
+    if (definitionFound == false) {
+      whileIndex = 0;
+    }
+    // Return label location
+    let myRange = new vscode.Range(document.lineAt(position.line - whileIndex).range.start, document.lineAt(position.line - whileIndex).range.end);
+    location = new vscode.Location(document.uri, myRange);
+    return location;
+  }
+  // Program definitions
+  else {
+    // Find program definition
+    regex = new RegExp(`PROGRAM\\s+${word}\\b`, "g");
+    match = regex.exec(document.getText());
+    if (match) {
+      const range = new vscode.Range(document.positionAt(match.index), document.positionAt(match.index + match[0].length));
+      // Return definition location
+      const location = new vscode.Location(document.uri, range);
+      return location;
+    }
+  }
+  return null;
+};
 
 module.exports = programDefinition;
